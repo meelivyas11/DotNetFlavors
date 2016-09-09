aaa933f45b3114b4c06bad006f12ee1176c10eeb
collab_mvyas <Feb-2016>
2016-09-09 15:15:50 -0400

    Mapper Config

---
+++ MVVC-App/App_Start/MapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using EntityFramework_ClassLibrary.Domain.Model;
+using MVVC_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVVC_App.App_Start
+{
+    public class MapperConfig
+    {
+        public static void Setup()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<AddPersonViewModel, PersonEntity>();
+
+                cfg.CreateMap<PersonEntity, SeePersonViewModel>()
+                .ForMember(desti => desti.PersonState, opt => opt.MapFrom(src => src.State));
+
+            });
+        }
+    }
+}
